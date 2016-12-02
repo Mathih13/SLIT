4324032dcba698ef7d9053c2dbaffa5fa9a107ec
Terje Heum Seljåsen <Terjeseljaasen@gmail.com>
2016-12-02 13:04:13 +0100

    Merge branch 'TerjeModulAdd'
    # Conflicts:
    #
    Slit-Server/Slit-Server-ejb/build/classes/Server/ModuleSessionBean.class

--- Slit-Client/src/java/slitclient/Main.java
+++ Slit-Client/src/java/slitclient/Main.java
@@ -10,31 +10,43 @@ import DataModel.StudentDataModel;
 import Framework.ModuleManager;
 import Framework.TeacherManager;
 import Framework.UserManager;
-import java.util.Date;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  *
  * @author Terje
  */
-public class Main {
+public class Main extends Application {
 
-    /**
-     * @param args the command line arguments
-     */
+       
+    @Override
+    public void start(Stage stage) throws Exception{
+        Parent root=FXMLLoader.load(getClass().getClassLoader().getResource("../ModuleCreate.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    
     public static void main(String[] args) {
         UserManager userManager = new UserManager(); 
         TeacherManager teacherManager = new TeacherManager();
         ModuleManager moduleManager = new ModuleManager();
         
+        launch(args);
         
-       
+    
         
         //userManager.insertUser(10, "John", "Dahl", "kaalsaas@outlook.com", "00000000", "kaalsaas", "Test123");
         
         //teacherManager.insertTeacher(33, "Torgeir", "Hansen", "Hansen@mailenhans.no", "4732839283", "Hansemann", "Passord123");
-        
-        //moduleManager.addModule("001", "Module1", "Renjogsnsd", "33", "massetekst", "goalss", new Date());
-        
+
+        //moduleManager.addModule("008", "Module1", "Renjogsnsd", "001", "massetekst", "goalss", "2017-09-09");
+        //moduleManager.addModule("10", "Module5", "MASSE TEKST REQUERMENTS",  "002", "MASSE TEKST DESCRIPTION", "MASSE TEKST GOALS", "2017-09-05");
+      
         ModuleDataModel outPutModel = moduleManager.getModel("001");
         
         if(outPutModel.getModuleID() != null){
@@ -45,10 +57,19 @@ public class Main {
         
         if(outPutStudent.getUserid() != 0){
             System.out.println(outPutStudent.getUserid());
+            
+       
         
         }
         
         
+        
     }
+
+
+    
+
     
 }
+
+
